45a6526b609559a7152f4f2a33fc69d1d4a73c9a
Mihir Shah <mihirs@cs.utexas.edu>
2023-03-22 13:35:36 -0500

    p4db emu doesn't crash :|

--- src/ee/database.hpp
+++ src/ee/database.hpp
@@ -83,9 +83,9 @@ public:
         msg_handler = std::make_unique<MessageHandler>(*this, comm.get());
         msg_handler->init.wait();
 		per_core_txns = (std::vector<Txn>**) malloc(sizeof(per_core_txns[0])*n_threads);
+        setup_sched_sock();
 
         if (!ORIG_MODE) {
-            setup_sched_sock();
             setup_switch_sock();
         }
     }
--- src/ee/defs.hpp
+++ src/ee/defs.hpp
@@ -34,10 +34,8 @@ constexpr auto STATS_CYCLE_SAMPLE_TIME = 10ms; //100us;
 constexpr auto STATS_PERIODIC_SAMPLE_TIME = 500ms;
 constexpr auto PERIODIC_CSV_FILENAME = "periodic.csv";
 constexpr bool DYNAMIC_IPS = false;
-constexpr bool USE_FLOW_ORDER = true;
-
-// if this is on, make sure to set MAX_PASSES_ACCEL=2, MAX_OPS_PASS2 high.
-constexpr bool ORIG_MODE = false;
+constexpr bool USE_FLOW_ORDER = false;
+constexpr bool ORIG_MODE = true;
 
 constexpr size_t N_CORES = 8;
 
@@ -53,12 +51,15 @@ constexpr bool DUMP_SWITCH_PKTS = false;
 constexpr int N_OPS = 8;
 constexpr size_t MAX_TIMES_ACCEL_ABORT = 1;
 constexpr size_t REMOTE_FRAC = 10;
-constexpr size_t BATCH_SIZE_TGT = 100000; // TODO need to be scaled as we change thread cts.
+constexpr size_t BATCH_SIZE_TGT = 100000;
 constexpr size_t MINI_BATCH_SIZE_TGT = 5000;
 constexpr size_t MIN_MINI_BATCH_THR_SIZE = 50;
-constexpr size_t MAX_PASSES_ACCEL = 1;
-constexpr size_t MAX_OPS_PASS2_ACCEL = 2;
+constexpr size_t MAX_PASSES_ACCEL = 2;
+constexpr size_t MAX_OPS_PASS2_ACCEL = 8;
 
 // measure and modify (right now around 1 ms each)
 constexpr uint64_t COLD_BATCH_DUR_EST_NS = 1000000ULL; 
 constexpr uint64_t HOT_BATCH_DUR_EST_NS = 1000000ULL;
+
+static_assert((!USE_FLOW_ORDER && MAX_PASSES_ACCEL == 2) || (USE_FLOW_ORDER && MAX_PASSES_ACCEL == 1));
+static_assert(!ORIG_MODE || (MAX_PASSES_ACCEL == 2 && MAX_OPS_PASS2_ACCEL == N_OPS));
